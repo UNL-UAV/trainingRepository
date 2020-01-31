d07cb269b276d707f185d5c42b11ba524deff191
Gerson Uriarte <Gersongru@huskers.unl.edu>
2020-01-30 19:39:51 -0600

    This is the beginning of the syntax tutorial that I am building, I'm pushing it now to make sure the .gitignore is correct.

---
+++ syntaxTraining.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //This is a method to print to the console using the namespace
+    cout << "Hello World!" << endl;
+
+    //This is a method to print to the console using the printf function
+    printf("Hellow World!\n");
+
+
+    return 0;
+}
